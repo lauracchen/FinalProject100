29d4ab40cec0972aae764cf4758f313e8b18a820
lcchen3 <lcchen3@mit.edu>
2022-04-25 14:28:52 -0400

    ownership data prep + activity data

--- agb_ownership.R
+++ agb_ownership.R
@@ -2,30 +2,31 @@
 library(rgdal) # package for geospatial analysis
 library(ggplot2) # package for plotting
 library(raster) # package for raster manipulation
-library(sf)
+library(sf) # package for geospatial df manipulation
+library(geojsonR) #package for geoJSON
+library(dplyr) #for data manipulation
 
 #set wd
 setwd("~/1.00/finalproject_100/")
 
 #import AGB, ownership, and activity data
 a = "MISR_agb_estimates_20002021.tif"
-o14 = "own2014.tif"
+o10 = "own2010.tif"
 o17 = "forest_own1.tif"
-o20 = "Public and Private Forest Ownership Conterminous United States (Image Service).tiff"
+anepa = "Activity_Project_Areas_NEPA_(Feature_Layer).geojson"
+#o20 = "Public and Private Forest Ownership Conterminous United States (Image Service).tiff"
 
 #agb_data <- stack(x) #GDALinfo(y)
-GDALinfo(o17)
-GDALinfo(o20)
 agb_10 <- raster(a, band = 11) 
-agb_20 <- raster(a, band = 21)
-own_14 <- raster(o14)
-own_20 <- raster(o20)
-
+agb_17 <- raster(a, band = 18)
+own_10 <- raster(o10)
+own_17 <- raster(o17)
+nepa <- st_read(anepa)
 
 #PREP AGB DATA
 #Calculate percent change
-agb_10m <- mask(agb_10,agb_20) #remove NAs from 2010
-perc_chng <- (agb_20-agb_10m)/agb_10m
+agb_10m <- mask(agb_10,agb_17) #remove NAs from 2010
+perc_chng <- (agb_17-agb_10m)/agb_10m
    #remove infs from percent change 
 change <- calc(perc_chng,fun = function(x) {x[x==Inf]<- NA; return(x)})
 
@@ -37,25 +38,47 @@ change <- calc(change,fun = function(x) {x[x<=(-0.05)]<- 1; return(x)})
 #Plot AGB
 par(mar = c(1, 1, 1, 1))
 pal <- colorRampPalette(c("red","green"))
-plot(change,col=pal(3), main ="Aboveground Biomass Change 2010-2020 [Mg/ha]")
+plot(change,col=pal(3), main ="Aboveground Biomass Change 2010-2017")
 
 #PREP OWNERSHIP DATA
 # import, establish dummy binary variables 
+# crop extents to match + remove NAs
+own10c <- crop(own_10, extent(-2361675,-285913.9,931625,1647599),snap="near")
+own17c <- crop(own_17, extent(own10c),snap="near")
+own_10m <- mask(own10c,own17c)
 
-own_17m <- mask(own_17,own_20)
+#change ownership to 0 vs. 1 -> public/private
+ot10 <- calc(own10c,fun = function(x) {x[x<=3]<- NA; return(x)}) #non-forest
+ot10 <- calc(ot10,fun = function(x) {x[x>=9&x<=19]<- 1; return(x)}) #private
+ot10 <- calc(ot10,fun = function(x) {x[x==20]<- 0; return(x)}) #public
 
-#PREP ACTIVITY DATA
+ot17 <- calc(own17c,fun = function(x) {x[x==8]<- NA; return(x)}) #exclude tribes
+ot17 <- calc(ot17,fun = function(x) {x[x<=3]<- 1; return(x)}) #private
+ot17 <- calc(ot17,fun = function(x) {x[x>=5&x<=7]<- 0; return(x)}) #public
+par(mar = c(1, 1, 1, 1))
+pal <- colorRampPalette(c("blue","red"))
+plot(ot17,col=pal(3), main ="Ownership 2017")
+
+#remove those that changed (i.e. difference is 1 or -1)
+#subtract 2017 from 2010 -> if 1 or -1 -> make NA
+#mask 2017 based off of 2010 NAs 
+
+
+#PREP ACTIVITY + COST DATA
 # import, establish dummy binary variables
-# intersect geometry w 
+# simplify to binary activity + costs
+# intersect geometry w raster
+#extract
+
+#filter only 2010-2017
+nepa$NEPA_SIGNED_DATE <- as.Date(nepa$NEPA_SIGNED_DATE)
+nepa1017<-filter(nepa,NEPA_SIGNED_DATE>"2010-01-01"&NEPA_SIGNED_DATE<"2018-01-01")
 
 
 
-plot(own_dem,
-     main = "Forest ownership")
-#hist(agb_20,
-    # main = "Histogram of Aboveground Biomass")
 
-#check for nas and remove
+
+
 #check for passing QA
 
 #predict? - predict how ownership change will affect forest carbon?
@@ -64,7 +87,6 @@ plot(own_dem,
 #in each, then aggregate
 #how much $
 
-#can use extract
 #can use as.logical
 
 
